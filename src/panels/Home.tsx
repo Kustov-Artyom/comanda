aacdb21e8edaf8d9ca74a0bd7deeb38da4f8b0f0
Kustov-Artyom <149572995+Kustov-Artyom@users.noreply.github.com>
2025-01-23 10:50:42 +0500

    add-ignore-file

--- src/panels/Home.tsx
+++ src/panels/Home.tsx
@@ -24,21 +24,7 @@ export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
   return (
     <Panel id={id}>
       <PanelHeader>Главная</PanelHeader>
-      {fetchedUser && (
-        <Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-          <Cell before={photo_200 && <Avatar src={photo_200} />} subtitle={city?.title}>
-            {`${first_name} ${last_name}`}
-          </Cell>
-        </Group>
-      )}
-
-      <Group header={<Header mode="secondary">Navigation Example</Header>}>
-        <Div>
-          <Button stretched size="l" mode="secondary" onClick={() => routeNavigator.push('persik')}>
-            Покажите Персика, пожалуйста!
-          </Button>
-        </Div>
-      </Group>
+      
     </Panel>
   );
 };
